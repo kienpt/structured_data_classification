2b5c835269413ac40d73dd6e5c505f01aed03f46
tuananh <tuananh@nyu.edu>
2016-04-14 16:26:19 -0400

    added Bunch

--- baselines/utils.py
+++ baselines/utils.py
@@ -1,19 +1,68 @@
+#!/usr/bin/python
 
+import os
+import json
+
+class Bunch(dict):
+    """Container object for datasets
+    Dictionary-like object that exposes its keys as attributes.
+    >>> b = Bunch(a=1, b=2)
+    >>> b['b']
+    2
+    >>> b.b
+    2
+    >>> b.a = 3
+    >>> b['a']
+    3
+    >>> b.c = 6
+    >>> b['c']
+    6
+    """
+
+    def __init__(self, **kwargs):
+        dict.__init__(self, kwargs)
+
+    def __setattr__(self, key, value):
+        self[key] = value
+
+    def __getattr__(self, key):
+        try:
+            return self[key]
+        except KeyError:
+            raise AttributeError(key)
+
+    def __setstate__(self, state):
+        # Bunch pickles generated with scikit-learn 0.16.* have an non
+        # empty __dict__. This causes a surprising behaviour when
+        # loading these pickles scikit-learn 0.17: reading bunch.key
+        # uses __dict__ but assigning to bunch.key use __setattr__ and
+        # only changes bunch['key']. More details can be found at:
+        # https://github.com/scikit-learn/scikit-learn/issues/6196.
+        # Overriding __setstate__ to be a noop has the effect of
+        # ignoring the pickled __dict__
+        pass
 
 def preprocess_text(text):
     return text
 
-def fetch_data(indir, label='positive', y_value=1):
+def fetch_data(indir, y_value=1):
     # return data with appropriate label
     # read and preprocess text, then assign label
+    data = []
+    target = []
+    for fname in os.listdir(indir):
+        for line in open(indir+'/'+fname):
+            data.append(preprocess_text(json.loads(line)['extract_text']))
+            target.append(y_value)
+    return Bunch(data=data, target=target)
 
 def split_train_test(positive_data, negative_data, ratio=0.5):
     # return train and test data
 
 
 def prepare_data(positive_dir, negative_dir):
-    positive_data = fetch_data(positive_dir, 'positive', 1)
-    negative_data = fetch_data(negative_dir, 'negative', -1)
+    positive_data = fetch_data(positive_dir, 1)
+    negative_data = fetch_data(negative_dir, 0)
     data_train, data_test = split_train_test(positive_data, negative_data, 0.5)
     return data_train, data_test
 
