059eba8de15ae81be140d0af572c90c064605d3e
Kien Pham <kienpt.vie@gmail.com>
2016-04-14 16:25:34 -0400

    add function to preprocess text

--- baselines/utils.py
+++ baselines/utils.py
@@ -1,14 +1,25 @@
-
+import re 
+pattern = re.compile('[\W_]+')
 
 def preprocess_text(text):
-    return text
+    #- lowercase
+    #- strip everything but numbers and chars
+
+    #- lowercase
+    clean_text = text.lower()
+    #- strip everything but numbers and chars
+    clean_text = pattern.sub('', clean_text)
+
+    return clean_text
 
 def fetch_data(indir, label='positive', y_value=1):
     # return data with appropriate label
     # read and preprocess text, then assign label
+    print ""
 
 def split_train_test(positive_data, negative_data, ratio=0.5):
     # return train and test data
+    print ""
 
 
 def prepare_data(positive_dir, negative_dir):
@@ -17,3 +28,7 @@ def prepare_data(positive_dir, negative_dir):
     data_train, data_test = split_train_test(positive_data, negative_data, 0.5)
     return data_train, data_test
 
+def test():
+    print preprocess_text("aDKJJGEflkk323*6ld^^^j")
+
+test()
