560e141620ab82a29979f810b09616cdb9200ebc
tuananh <tuananh@nyu.edu>
2016-04-22 19:37:47 -0400

    added initial urls

--- data_collection/google/google_cse_multiclass.py
+++ data_collection/google/google_cse_multiclass.py
@@ -5,25 +5,33 @@ import json
 import time
 from config import *
 
-def get_results(query, start=1):
-    r = requests.get('https://www.googleapis.com/customsearch/v1?key='+api_key+'&cx='+cse_uid+'&q='+query+'&start='+str(start))
+def get_results(cse_uid, query, start=1):
+    url = 'https://www.googleapis.com/customsearch/v1?key='+api_key+'&cx='+cse_uid+'&q='+query+'&start='+str(start)
+    # print url
+    # return url
+    r = requests.get(url)
     if r.status_code == 200:
         return r.json()
     else:
         print r.text
         return None
 
-# print get_results('pizza')
+cse_uids = map(str.strip, open('cse.txt').readlines())
+cse_uids = [x.split(':') for x in cse_uids]
+cse_uids = {x[0]: x[1] for x in cse_uids}
 
-fout = open('results-8-12.json', 'w')
+keywords = map(str.strip, open('../dmoz/data/alltopics_splitted.txt').readlines())
+keywords = [x.split(',') for x in keywords]
+keywords = {x[0].replace(' ', ''):x for x in keywords}
 
-keywords = map(str.strip, open('keywords.txt').readlines())
-for i in range(8,12):
-    start = 1+i*10
-    for q in keywords:
-        print "Querying", q, "and start =", start
-        data = get_results(q, start)
-        # data = {'q':q, 'start':start}
-        fout.write(json.dumps(data) + '\n')
-        fout.flush()
-        time.sleep(1)
+for topic, uid in cse_uids.iteritems():
+    fout = open('data/'+topic.lower()+'.json', 'w')
+    kws = keywords[topic]
+    for i in range(0, 5):
+        start = 1+i*10
+        for q in kws:
+            print "Querying", q, "and start =", start
+            data = get_results(cse_uid+':'+uid, q, start)
+            fout.write(json.dumps(data) + '\n')
+            fout.flush()
+            time.sleep(1)
