134404f9e94fbb88a8ba8728aa048ba7a148e279
Kien Pham <kienpham@vidaserver1.poly.edu>
2016-04-22 17:23:17 -0400

    prepare data for multiclass classification

--- baselines/utils.py
+++ baselines/utils.py
@@ -73,6 +73,39 @@ def fetch_data(indir, y_value=1):
             target.append(y_value)
     return Bunch(data=data, target=np.array(target))
 
+def fetch_data_multiclass(indir):
+    data = []
+    target = []
+    if os.path.isdir(indir):
+        for fname in os.listdir(indir):
+            for line in open(indir+'/'+fname):
+                extracted_text = json.loads(line)
+                data.append(preprocess_text(obj['extract_text']))
+                target.append(obj['topic'])
+    else:
+        for line in open(indir):
+           extracted_text = json.loads(line)
+           data.append(preprocess_text(obj['extract_text']))
+           target.append(obj['topic'])
+
+    return Bunch(data=data, target=np.array(target))
+
+def split_train_test_multiclass(all_data, ratio=0.5):
+    #shuffle
+    random_state = check_random_state(42)
+    indices = np.arange(all_data.target.shape[0])
+    random_state.shuffle(indices)
+    all_data.target = all_data.target[indices]
+    # Use an object array to shuffle: avoids memory copy
+    data_lst = np.array(all_data.data, dtype=object)
+    data_lst = data_lst[indices]
+    all_data.data = data_lst.tolist()
+
+    split_point = int(all_data.target.shape[0] * ratio)
+    data_train = Bunch(data = all_data.data[:split_point], target = all_data.target[:split_point])
+    data_test = Bunch(data = all_data.data[split_point:], target = all_data.target[split_point:])
+    return data_train, data_test
+
 def split_train_test(positive_data, negative_data, ratio=0.5):
     # return train and test data
 
@@ -96,10 +129,15 @@ def split_train_test(positive_data, negative_data, ratio=0.5):
     data_test = Bunch(data = all_data.data[split_point:], target = all_data.target[split_point:])
     return data_train, data_test
 
-def prepare_data(positive_dir, negative_dir, ratio=0.5):
+def prepare_data_oneclass(positive_dir, negative_dir, ratio=0.5):
     positive_data = fetch_data(positive_dir, 1)
     negative_data = fetch_data(negative_dir, 0)
     data_train, data_test = split_train_test(positive_data, negative_data, ratio)
+    return data_train, data_test
+
+def prepare_data_multiclass(positive_dir, ratio-0.5):
+    data = fetch_data_multiclass(positive_dir)
+    data_train, data_test = split_train_test_multiclass(data)
     return data_train, data_test
 
 def test_preprocess():
